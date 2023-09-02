0e30ef95e08cf8a6ad66f9248597016ded6f78c2
Rogerio Angeliski <rogerio.angeliski@rdstation.com>
2023-09-02 10:12:39 -0300

    fix tailwind compile bevahior

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ bundle check > /dev/null 2>&1 || bundle install -j4
 if [ "$#" == 0 ]
 then
   bundle exec rake db:create db:migrate
-  rails tailwindcss:watch
-  exec bundle exec rails server -b '0.0.0.0'
+  ./bin/dev
 fi
 exec $@
